75037cc073ade9966ccf1079e746b7e926e2b626
jobiols <jorge.obiols@gmail.com>
2017-11-01 11:27:07 -0300

    [FIX] chequear DNI con carcteres no numericos - ademas faltaba un import pareceria que estos chequeos no andaban

--- partner_fiscal_constraints/models/res_partner.py
+++ partner_fiscal_constraints/models/res_partner.py
@@ -6,6 +6,7 @@
 
 from openerp import models, api
 from openerp.exceptions import ValidationError
+import re
 
 
 class ResPartner(models.Model):
@@ -21,17 +22,6 @@ class ResPartner(models.Model):
                 if partner.document_type_id.name != 'CUIT':
                     raise ValidationError(u'Para ingresar un cliente "{}" Se requiere CUIT'.
                                           format(partner.responsability_id.name))
-
-    @api.multi
-    @api.constrains('document_number')
-    def _check_document_type(self):
-        for partner in self:
-            # verifica que el DNI sea numerico
-            if partner.document_type_id.name == 'DNI':
-                try:
-                    int(partner.document_number) + 1
-                except:
-                    raise ValidationError(u'El DNI debe contener solo numeros')
 
     @api.multi
     @api.constrains('document_number')
@@ -53,8 +43,17 @@ class ResPartner(models.Model):
                             partner.vat[2:4], partner.vat[4:12], partner.vat[12:13]))
 
     @api.multi
-    @api.constraints('responsability_id')
+    @api.constrains('responsability_id')
     def _check_address_exist(self):
         for partner in self:
             if not partner.street and partner.responsability_id.code == '1':
                 raise ValidationError('Un responsable inscripto debe tener direccion')
+
+    @api.multi
+    @api.constrains('document_number')
+    def _check_document_type(self):
+        for partner in self:
+            # verifica que el DNI sea numerico
+            if partner.document_type_id.name == 'DNI':
+                if partner.document_number != re.sub("[^0-9]", "", partner.document_number):
+                    raise ValidationError('El DNI "{}" debe contener solo numeros'.format(partner.document_number))
