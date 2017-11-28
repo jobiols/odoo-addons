c017865ec9029a5511250d368c9341ce2476aef6
jobiols <jorge.obiols@gmail.com>
2017-11-28 19:13:03 -0300

    [FIX] travis dependecies

---
+++ odoo2odoo/__openerp__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015  JEOSOFT  (http://www.jeosoft.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Odoo to Odoo',
+    'version': '9.0.1.0.0',
+    'category': 'Tools',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Replicate data between Odoo Databases
+=====================================
+
+    """,
+    'author':  'jeo Soft',
+    'website': 'jeosoft.com.ar',
+    'license': 'AGPL-3',
+    'images': [
+    ],
+    'depends': [
+        'connector'
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
