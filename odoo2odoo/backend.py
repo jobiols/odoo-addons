23432f6b7c4905ee7bcc92dc62bf594e2e03d9f4
jobiols <jorge.obiols@gmail.com>
2017-12-29 13:39:51 -0300

    [REM] odoo2odoo module

--- odoo2odoo/backend.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-import openerp.addons.connector.backend as backend
-
-odoo_base = backend.Backend('odoo_base')
-odoo90 = backend.Backend(parent=odoo_base, version='9.0')
