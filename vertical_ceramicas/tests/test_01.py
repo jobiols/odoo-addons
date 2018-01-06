6a03dac59b0e5a4d7ebfefc3caf86d8410d6bf55
jobiols <jorge.obiols@gmail.com>
2018-01-06 15:58:56 -0300

    [REF] mejorar linter

--- vertical_ceramicas/tests/test_01.py
+++ vertical_ceramicas/tests/test_01.py
@@ -4,9 +4,10 @@
 # directory
 ##############################################################################
 # Forma de correr el test
-# Crear un cliente test, una bd test_pricelist, el modulo test_pricelist_import cargado y
-# un usuario admin / admin
-# OJO cambiarle el path donde está el archivo a importar, esta en lugares distintos para travis o local
+# Crear un cliente test, una bd test_pricelist, el modulo
+# test_pricelist_import cargado y un usuario admin / admin
+# OJO cambiarle el path donde está el archivo a importar, esta en
+# lugares distintos para travis o local
 # ./odooenv.py -Q reves test_01.py -c reves -d reves_travis -m reves_default
 from openerp.tests.common import SingleTransactionCase
 
