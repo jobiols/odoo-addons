5fd070561f4160a8f976d8afc372355245878422
jobiols <jorge.obiols@gmail.com>
2018-02-06 21:24:43 -0300

    [ADD] stock_picking_auto initial commit

---
+++ stock_picking_auto/README.rst
@@ -0,0 +1,12 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+    :alt: License: AGPL-3
+
+=======================
+Stock picking automatic
+=======================
+
+Provide an automatic picking when a Invoice is validated.
+
+
+
---
+++ stock_picking_auto/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Stock picking automatic',
+    'version': '9.0.0.0',
+    'category': 'Warehouse Management',
+    'author': "jeo Software",
+    'website': 'http://www.jeosoft.com.ar',
+    'depends': [
+
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': "AGPL-3",
+}
