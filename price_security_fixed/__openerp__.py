58e8ff96c200751afc4c9f80d09c15ac5247b451
jobiols <jorge.obiols@gmail.com>
2018-04-05 12:39:26 -0300

    [FIX] price_security --> price_security_fixed cambie el nombre de un par de funciones constrains

--- price_security_fixed/__openerp__.py
+++ price_security_fixed/__openerp__.py
@@ -22,7 +22,7 @@
     'name': 'Price Security',
     'version': '9.0.1.3.1',
     'category': 'Sales Management',
-    'author': 'jeoSoftware ADHOC SA, Odoo Community Association (OCA)',
+    'author': 'jeoSoftware, ADHOC SA, Odoo Community Association (OCA)',
     'website': 'http://www.adhoc.com.ar/',
     'license': 'AGPL-3',
     'depends': [
