a14e719d4a080d5a7e02d56f08df71ae45251890
jobiols <jorge.obiols@gmail.com>
2018-04-11 21:00:08 -0300

    [FIX] - add moossssssssssssssooooore logging

--- product_autoload/models/autoload.py
+++ product_autoload/models/autoload.py
@@ -96,26 +96,31 @@ class AutoloadMgr(models.Model):
                         if prod:
                             prod.recalculate_list_price(item.margin)
                             _logger.info('recalculate for modify \n'
-                                         '[{}] - [{}]\n'
-                                         '[{}] - [{}]\n'
-                                         '[{}] - [{}]\n'
-                                         '[{}] - [{}]\n'
-                                         '[{}] - [{}]\n'.format(
+                                         '[{}] - [{}] {}\n'
+                                         '[{}] - [{}] {}\n'
+                                         '[{}] - [{}] {}\n'
+                                         '[{}] - [{}] {}\n'
+                                         '[{}] - [{}] {}\n'.format(
                                 item.name,
                                 values['name'],
+                                item.margin == values['margin'],
 
                                 item.origin,
                                 values['origin'],
+                                item.origin == values['origin'],
 
                                 item.section,
                                 values['section'],
+                                item.section == values['section'],
 
                                 item.family,
                                 values['family'],
+                                item.family == values['family'],
 
                                 item.margin,
                                 values['margin'],
-                            ))
+                                item.margin == float(values['margin'])
+                                ))
                 else:
                     item_obj.create(values)
 
