a7b326844ee42095c87959ac6b2228595eddc470
jobiols <jorge.obiols@gmail.com>
2018-05-20 18:11:14 -0300

    [FIX] reporte de cajas, version '9.0.2.0.1'

--- account_cash_report/__openerp__.py
+++ account_cash_report/__openerp__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Account Cash Report',
-    'version': '9.0.2.0',
+    'version': '9.0.2.0.1',
     'license': 'AGPL-3',
     'category': 'Accounting',
     'summary': 'Reporte diario para cierre de cajas',
@@ -11,10 +11,10 @@
         'account',
     ],
     'data': [
+        'views/cash_view.xml',
         'wizard/cashier_report_view.xml',
         'views/cashier_report.xml',
         'views/account_journal_view.xml',
-        'views/cash_view.xml',
         'views/res_users_view.xml'
     ],
     'test': [
