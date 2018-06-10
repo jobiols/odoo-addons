e7c20b83377c31a9abf4d78b99282844a37e13ac
jobiols <jorge.obiols@gmail.com>
2018-06-10 16:13:18 -0300

    [ADD] partner_search NEED UPGRADE

---
+++ partner_search/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------------
+#
+#    Copyright (C) 2016  jeo Software  (http://www.jeosoft.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# -----------------------------------------------------------------------------------
+{
+    "name": "Partner search",
+    "version": "8.0.1.0",
+    "author": "jeo Software",
+    'website': 'http://www.jeosoft.com.ar',
+    "depends": ["l10n_ar_base"],
+    "category": "Generic Modules",
+    "description": """
+Allows searching form multiple fields
+""",
+    "demo_xml": [],
+    "data": [
+        'views/partner_view.xml'
+    ],
+    'installable': False,
+    'application': True,
+    'auto_install': False,
+    'images': [],
+}
