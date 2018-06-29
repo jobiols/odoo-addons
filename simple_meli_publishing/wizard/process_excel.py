08d9993c9ec2b80a4041d67f85bc3ea5bd1d91eb
jobiols <jorge.obiols@gmail.com>
2018-06-29 17:36:01 -0300

    [ADD] simple_meli_publishing released

--- simple_meli_publishing/wizard/process_excel.py
+++ simple_meli_publishing/wizard/process_excel.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from openerp import api, models, fields
-from openerp.exceptions import UserError, Warning
+from openerp import api, models, fields, _
 import base64
 import tempfile
 import openpyxl
@@ -27,37 +26,75 @@ class SimpleMeliPublishing(models.TransientModel):
          ('error', 'Error')],  # show errors
         default="load"
     )
+    errors = fields.Html(
+        default='<h1>We found the following errors</h1>',
+        readonly=True
+    )
 
+    @api.multi
+    def add_error(self, tag, row=False, meli_code=False, sku=False,
+                  default_code=False):
+        for rec in self:
+            if tag == 'not_found':
+                rec.errors += _('<p>The product code %s from row %s on the '
+                                'worksheet can not be found in the system.'
+                                '</p>') % (meli_code, row)
+            if tag == 'sku':
+                rec.errors += _('<p>The sku from worksheet does not match with'
+                                ' the product internal reference "%s" <> "%s" '
+                                'at row %s in the worksheet.'
+                                '</p>') % (sku, default_code, row)
+
+    @api.one
     def process_data(self, fp_name):
+        PUB_CODE_COL = 2
+        PRICE_COL = 7
+        SKU_COL = 1
+        FIRST_ROW = 4
+
         product_obj = self.env['product.product']
 
         # open worksheet
         wb = openpyxl.load_workbook(filename=fp_name,
                                     read_only=False,
                                     data_only=True)
-        wb.active
+        sheet = wb.active
+        for row in range(FIRST_ROW, sheet.max_row):
+            meli_code = sheet.cell(column=PUB_CODE_COL, row=row).value
+            sku = sheet.cell(column=SKU_COL, row=row).value
+            prod = product_obj.search([('meli_code', '=', meli_code)])
+            if prod:
+                if sku != prod.default_code:
+                    self.state = 'error'
+                    self.add_error('sku', row=row, sku=sku,
+                                   default_code=prod.default_code)
+                else:
+                    sheet.cell(column=PRICE_COL, row=row,
+                               value=prod.final_price)
+            else:
+                self.state = 'error'
+                self.add_error('not_found', row=row, meli_code=meli_code)
 
+        if self.state != 'error':
+            wb.save(fp_name)
 
     @api.multi
     def load_file(self):
         for rec in self:
-            #import wdb;wdb.set_trace()
 
             # escribir la data en un archivo temporario
             data = base64.decodestring(rec.data)
             (fileno, fp_name) = tempfile.mkstemp('.xlsx', 'openerp_')
-#            openfile = open(fp_name, "w")
-#            openfile.write(data)
-#            openfile.close()
             with open(fp_name, "w") as worksheet:
                 worksheet.write(data)
 
             # procesar el archivo
-            #self.process_data(fp_name)
+            self.process_data(fp_name)
 
             # leer el archivo temporario para hacer download
             with open(fp_name, "r") as worksheet:
                 rec.pdata = base64.encodestring(worksheet.read())
 
             # cambiar la vista para que descarguen el archivo
-            rec.state = 'download'
+            if rec.state != 'error':
+                rec.state = 'download'
