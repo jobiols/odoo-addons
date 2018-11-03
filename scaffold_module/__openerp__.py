8a42979af5f29790e9f058e371e38e4e00e0c445
jobiols <jorge.obiols@gmail.com>
2018-11-03 16:44:49 -0300

    [ADD] scaffold module

---
+++ scaffold_module/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# -*- coding: utf-8 -*-
+# Copyright 2015 GRAP - Sylvain LE GAL
+# Copyright 2017 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Module Name',
+    'version': '9.0.0.0.0',
+    'category': 'Tools',
+    'summary': "Module summary",
+    'author': "jeo Software",
+    'website': 'http://github.com/jobiols/module-repo',
+    'license': 'AGPL-3',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+}
