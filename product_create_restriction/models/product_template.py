64712439b04ef8f88d5133f509fd540fb36dc6f5
jobiols <jorge.obiols@gmail.com>
2019-04-06 17:00:49 -0300

    [FIX] enable create products to admin

--- product_create_restriction/models/product_template.py
+++ product_create_restriction/models/product_template.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # For copyright and license notices, see __manifest__.py file in module root
 
-from openerp import models, api, _
+from openerp import models, api, _, SUPERUSER_ID
 from openerp.exceptions import AccessError
 
 
@@ -12,7 +12,7 @@ class ProductTemplate(models.Model):
     def create(self, vals):
         """ No permitir la creacion de productos a cualquiera.
         """
-        if self.env.user.has_group(
+        if self.env.user.id == SUPERUSER_ID or self.env.user.has_group(
                 'product_create_restriction.group_product_create_users'):
             return super(ProductTemplate, self).create(vals)
         else:
