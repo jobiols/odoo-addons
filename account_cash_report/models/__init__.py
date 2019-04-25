96f35c289bad23aff34571889a01e963de5890ba
jobiols <jorge.obiols@gmail.com>
2019-04-25 12:41:06 -0300

    [REM] quitar los modulos que no estan migrados

--- account_cash_report/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import cashier_report_search
-from . import invoice_report_search
-from . import account_journal
-from . import cash
-from . import res_users
