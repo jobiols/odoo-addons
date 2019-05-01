aa61312929fee1b7dbc2a396e7dc933fbda54829
jobiols <jorge.obiols@gmail.com>
2019-05-01 16:53:44 -0300

    [FIX] fase bank terminada

--- point_of_sale_fast_close/models/account_bank_statement.py
+++ point_of_sale_fast_close/models/account_bank_statement.py
@@ -15,42 +15,57 @@ class AccountBankStatement(models.Model):
     _inherit = "account.bank.statement"
 
     @api.multi
-    def button_confirm_bank_stepped(self, stc):
+    def button_confirm_bank_stepped(self, pstc):
         """ Hace lo mismo que button confirm bank pero por pasos
         """
+        print('button_confirm_bank_stepped', pstc.phase, pstc.step)
         # hacer el balance solo en el primer step
-        if stc.step == 1:
+        if pstc.step == 1:
+            print('solo en el paso 1 hacer balance')
             self._balance_check()
 
         statements = self.filtered(lambda r: r.state == 'open')
         for statement in statements:
             moves = self.env['account.move']
+            # seleccionar las line_ids que no tengan st_line.journal_entry_ids
+            # son las que no se procesaron en pasos anteriores
+            lines = statement.line_ids.filtered(
+                lambda r:r.journal_entry_ids.ids == [])
 
-            # traer solo 100 lineas
-            lines = statement.line_ids.search([], limit=2)
+            # de estas lineas traer las primeras 100
+            lines = lines[:3]
+            print('traerse ', lines.ids, 'lineas para procesar')
             if lines:
-                stc.step += 1  # siguiente step
+                pstc.step += 1  # siguiente step
+                print('pasar a siguiente step', pstc.step)
             else:
-                stc.write({'step': 0, 'phase': 'orders'})  # termina
+                print('no hay mas lineas poner fin')
+                pstc.write({'step': 0, 'phase': 'orders'})  # termina
 
             for st_line in lines:
+                print('procesar una linea ----- ', st_line.id)
                 if st_line.account_id and not st_line.journal_entry_ids.ids:
                     st_line.fast_counterpart_creation()
-                elif not st_line.journal_entry_ids.ids and not statement.currency_id.is_zero(st_line.amount):
+                elif not st_line.journal_entry_ids.ids and \
+                    not statement.currency_id.is_zero(st_line.amount):
+                    print('ACA LANZA UN ERROR DE USUARIO')
                     raise UserError(_(
-                        'All the account entries lines must be processed in order to close the statement.'))
+                        'All the account entries lines must be processed '
+                        'in order to close the statement.'))
                 for aml in st_line.journal_entry_ids:
                     moves |= aml.move_id
             if moves:
+                print('procesar ', moves.ids, 'lineas de moves')
                 moves.filtered(lambda m: m.state != 'posted').post()
 
-            if stc.step == 1:
+            if pstc.step == 1:
+                print('solo en el paso 1 escribir en message')
                 statement.message_post(body=_(
                     'Statement %s confirmed, journal items were created.') % (
-                                            statement.name,))
-
+                                                statement.name,))
         # termina cerrar el proceso
-        if stc.step == 0:
+        if pstc.step == 0:
+            print('cerrando el proceso, es el ultimo paso')
             statements.link_bank_to_partner()
             statements.write({'state': 'confirm',
                               'date_done': time.strftime("%Y-%m-%d %H:%M:%S")})
--- point_of_sale_fast_close/models/pos_session.py
+++ point_of_sale_fast_close/models/pos_session.py
@@ -3,9 +3,12 @@
 
 from odoo import api, fields, models, SUPERUSER_ID, _
 from odoo.exceptions import UserError, ValidationError
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-class SessionToCloseQueue(models.Model):
+class PosSessionToClose(models.Model):
     _name = 'pos.session.to.close'
 
     session_id = fields.Many2one(
@@ -24,14 +27,27 @@ class PosSession(models.Model):
 
     @api.multi
     def action_pos_session_closing_control(self):
-        """ Llamado desde el boton de cerrar la sesion, si tengo cash control
-            funciona normalmente, si no lo tengo, cierro la sesion y la
-            almaceno en la cola de procesos.
+        """ Esto sobreescribe el metodo que se llama desde el boton de cerrar
+            la sesion.
+            Si tengo cash control llama al super y termina, o sea anda
+            como antes.
+            Si no lo tengo, cierro la sesion y la almaceno en la cola de
+            sesiones a procesar.
         """
-        # si tengo cash control funciona normalmente
+        # si tengo cash control tiene funcionamiento standard
         for session in self:
             if session.config_id.cash_control:
                 super(PosSession, self).action_pos_session_closing_control()
+
+        # chequear que el POS tenga un Sale Journal
+            company_id = session.config_id.journal_id.company_id.id
+            journal_id = self.env['ir.config_parameter'].sudo().get_param(
+                'pos.closing.journal_id_%s' % company_id,
+                default=session.config_id.journal_id.id)
+            if not journal_id:
+                raise UserError(
+                    _("You have to set a Sale Journal for the POS:%s") % (
+                        session.config_id.name,))
 
         # no tengo cash control solo cierra y prepara proceso batch
         self._check_pos_session_balance()
@@ -47,44 +63,70 @@ class PosSession(models.Model):
 
     @api.multi
     def post_entries(self):
-        """ Llamado desde cron para procesar en background
+        """ Llamado desde cron para procesar en background una sesion, que esta
+            almacenada en PosSessionToClose.
+
+            Cada elemento representa una sesion a cerrar, y tiene phase indica
+            que cosa estamos haciendo y step que incrementa cada vez que pasa
+            hasta que termina.
+
         """
-        # todavia no quiero que funcione esto.
-        return
 
         sq_obj = self.env['pos.session.to.close']
-        stc = sq_obj.search([], limit=1)
+        pstc = sq_obj.search([], limit=1)
 
-        if stc:
-            company_id = stc.session_id.config_id.company_id.id
+        if pstc:
+            logger.info(
+                'POS processing %s Phase %s Step %s' % (
+                    pstc.session_id.name, pstc.phase, pstc.step))
+
+            company_id = pstc.session_id.config_id.company_id.id
             ctx = dict(self.env.context, force_company=company_id,
                        company_id=company_id)
 
-            if stc.phase == 'bank':
-                for st in stc.session_id.statement_ids:
-                    st.with_context(ctx).button_confirm_bank_stepped(stc)
+            # ejecutar la fase banco
+            if pstc.phase == 'bank':
+                print('ejecutando fase banco')
+                for st in pstc.session_id.statement_ids:
+                    st.with_context(ctx).button_confirm_bank_stepped(pstc)
 
-            if stc.phase == 'orders':
-                stc.session_id.with_context(ctx)._confirm_orders_stepped(stc)
+            # ejecutar la fase ordenes
+            if pstc.phase == 'orders':
+                print('ejecutando fase ordenes')
+                pstc.session_id.with_context(ctx)._confirm_orders_stepped(pstc)
 
-    def _confirm_orders_stepped(self, stc):
+    def _confirm_orders_stepped(self, pstc):
         """ hace lo mismo que _confirm_orders pero por pasos
         """
+        import wdb;wdb.set_trace()
+
         for session in self:
             company_id = session.config_id.journal_id.company_id.id
-            orders = session.order_ids.filtered(lambda order: order.state == 'paid')
-            journal_id = self.env['ir.config_parameter'].sudo().get_param(
-                'pos.closing.journal_id_%s' % company_id, default=session.config_id.journal_id.id)
-            if not journal_id:
-                raise UserError(_("You have to set a Sale Journal for the POS:%s") % (session.config_id.name,))
 
-            move = self.env['pos.order'].with_context(force_company=company_id)._create_account_move(session.start_at, session.name, int(journal_id), company_id)
-            orders.with_context(force_company=company_id)._create_account_move_line(session, move)
-            for order in session.order_ids.filtered(lambda o: o.state not in ['done', 'invoiced']):
+            # traerse las ordenes en estado pago (y algo mas)
+            orders = session.order_ids.filtered(
+                lambda order: order.state == 'paid')
+
+            # quedarse con las primeras tres
+            orders = orders[:3]
+
+            journal_id = self.env['ir.config_parameter'].sudo().get_param(
+                'pos.closing.journal_id_%s' % company_id,
+                default=session.config_id.journal_id.id)
+
+            move = self.env['pos.order'].with_context(
+                force_company=company_id)._create_account_move(
+                session.start_at, session.name, int(journal_id), company_id)
+            orders.with_context(
+                force_company=company_id)._create_account_move_line(session,
+                                                                    move)
+            for order in session.order_ids.filtered(
+                lambda o: o.state not in ['done', 'invoiced']):
                 if order.state not in ('paid'):
                     raise UserError(
-                        _("You cannot confirm all orders of this session, because they have not the 'paid' status.\n"
-                          "{reference} is in state {state}, total amount: {total}, paid: {paid}").format(
+                        _(
+                            "You cannot confirm all orders of this session, because they have not the 'paid' status.\n"
+                            "{reference} is in state {state}, total amount: {total}, paid: {paid}").format(
                             reference=order.pos_reference or order.name,
                             state=order.state,
                             total=order.amount_total,
