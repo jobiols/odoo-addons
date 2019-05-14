978f8028bff7eaa4ffa6d08f7bc1dee35a351f63
jobiols <jorge.obiols@gmail.com>
2019-05-14 12:53:53 -0300

    [ADD] auth_api_key

---
+++ auth_api_key/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Auth Api Key",
+    "summary": """
+        Authenticate http requests from an API key""",
+    "version": "11.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "jeo Software, ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://jeosoft.com.ar",
+    "development_status": "Beta",
+    "depends": ["server_environment"],
+    "data": [],
+    "demo": [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
