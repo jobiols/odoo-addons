cf4eeaa44c4c6e1b1422e394fe9677ea68ed071e
jobiols <jorge.obiols@gmail.com>
2019-07-15 01:07:05 -0300

    [WIP] previsiones 1

--- cash_flow/models/forecast.py
+++ cash_flow/models/forecast.py
@@ -5,49 +5,62 @@ from odoo import fields, models, api
 
 class AccountInvoice(models.Model):
     _name = "cash_flow.forecast"
+    _inherit = ['mail.thread', 'mail.activity.mixin', 'portal.mixin']
+    _description = 'Forecasts'
 
     forecast_date = fields.Date(
         readonly=True,
-        states={'foreseen': [('readonly', False)]},
+        states={'draft': [('readonly', False)],
+                'foreseen': [('readonly', False)]},
         index=True,
         help="Forecast date, it will be used in the cash flow",
-        track_visibility='onchange'
+        track_visibility='onchange',
+        required=True
     )
 
     amount = fields.Monetary(
         string='Forecasted amount',
         currency_field='currency_id',
         readonly=True,
-        states={'foreseen': [('readonly', False)]},
+        states={'draft': [('readonly', False)],
+                'foreseen': [('readonly', False)]},
         help="Total Forecasted amount without taxes",
-        track_visibility='onchange'
+        track_visibility='onchange',
+        required=True
     )
-    description = fields.Char(
+    description = fields.Text(
 
     )
     type = fields.Selection([
         ('out', 'Expenses Forecast'),
         ('in', 'Incomes Forecast')],
+        readonly=True,
+        states={'draft': [('readonly', False)],
+                'foreseen': [('readonly', False)]},
         help='Forecast type',
-        track_visibility='onchange'
+        track_visibility='onchange',
+        default='out'
     )
 
     user_id = fields.Many2one(
         'res.users',
         string='User',
         readonly=True,
+        states={'draft': [('readonly', False)],
+                'foreseen': [('readonly', False)]},
         default=lambda self: self.env.user,
         track_visibility='onchange',
     )
 
     state = fields.Selection(
-        [('foreseen', 'Foreseen'),
+        [('draft', 'Draft'),
+         ('foreseen', 'Foreseen'),
          ('invoiced', 'Invoiced')],
         string='Status',
         index=True,
         readonly=True,
         help='Forecast state',
-        default='foreseen',
+        default='draft',
         track_visibility='onchange',
     )
 
@@ -63,3 +76,8 @@ class AccountInvoice(models.Model):
         states={'foreseen': [('readonly', False)]},
         default=_default_currency,
         track_visibility='always')
+
+    @api.multi
+    def name_get(self):
+        return [(fc.id, "%s - %s - %s" % (
+            fc.forecast_date, fc.amount, fc.type)) for fc in self]
