b172afda57cef19c9fa458eaef8566796aa8983c
jobiols <jorge.obiols@gmail.com>
2019-09-13 18:15:27 -0300

    author

--- scaffold_module/__manifest__.py
+++ scaffold_module/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright <YEAR(S)> <AUTHOR(S)>
+# Copyright 2019 jeo Software
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Module name",
