060b2e717a87f78a5341277be876228bea837c26
jobiols <jorge.obiols@gmail.com>
2019-11-06 10:45:47 -0300

    [FIX] travis

--- standard_depends_ce/__manifest__.py
+++ standard_depends_ce/__manifest__.py
@@ -37,23 +37,19 @@
         'account_debt_management',  # mejoras en administracion de deudas
         'l10n_ar_aeroo_payment_group',  # Pagos
         'l10n_ar_account_withholding',  # Retenciones
-        #'account_accountant',           # permisos para contabilidad
 
         # modulos adicionales utilitarios CE
         'base_ux',  # mejoras de base
         'account_ux',  # hace pilas de cosas ver en el modulo
         'sale_ux',  # mejoras en ventas
         'disable_odoo_online',  # Elimina menu de odoo online
-        #'res_config_settings_enterprise_remove',  # Elimina menus enterprise
+        # 'res_config_settings_enterprise_remove', # Elimina menus enterprise
         'auto_backup',  # poner el backup en: /var/odoo/backups/
         'server_mode',          # server_mode = "test" en desarrollo
         'partner_ref_unique',  # evita duplicados en referencia
         'partner_vat_unique',  # evita duplicados numeros de referencia
         'mail_activity_board_ux',  # quitar actividades del chatter
         'product_unique',  # no se pueden duplicar codigos de producto
-
-        # fixes
-
     ],
     'data': [],
     'demo': [],
