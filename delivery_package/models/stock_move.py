d13e68f659ecbf562d220cc4f18b9831e7939762
jobiols <jorge.obiols@gmail.com>
2019-11-17 01:17:01 -0300

    [FIX] agregar paquetes en el remito

--- delivery_package/models/stock_move.py
+++ delivery_package/models/stock_move.py
@@ -3,10 +3,35 @@
 from odoo import fields, models
 
 
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    packages = fields.Integer(
+        compute="_compute_packages"
+    )
+
+    def _compute_packages(self):
+        for rec in self:
+            pack = 0
+            for line in rec.move_lines:
+                pack += line.packages
+            rec.packages = pack
+
+
 class StockMove(models.Model):
     _inherit = "stock.move"
 
     packages = fields.Integer(
         'Paquete',
         help='Cantidad de elementos en el bulto'
+    )
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    packages = fields.Integer(
+        related="move_id.packages",
+        help='Cantidad de elementos en el bulto',
+        readonly=True
     )
