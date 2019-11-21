9086fa7788758c0e0177994da26b0db2cfb9e1cd
jobiols <jorge.obiols@gmail.com>
2019-11-21 17:00:48 -0300

    [FIX] corregir scaffold_module

--- scaffold_module/tests/test_something.py
+++ scaffold_module/tests/test_something.py
@@ -1,6 +1,25 @@
 # Copyright <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+#    Forma de correr el test
+#    -----------------------
+#
+#   Definir un subpackage tests que será inspeccionado automáticamente por
+#   modulos de test los modulos de test deben enpezar con test_ y estar
+#   declarados en el __init__.py, como en cualquier package.
+#
+#   Hay que crear una base de datos no importa el nombre pero se sugiere
+#   [nombre cliente]_test_[nombre modulo] que debe estar vacia pero con el
+#   modulo que se quiere testear instalado.
+#
+#   Debe tener usuario admin y password admin y demo data
+#
+#   Arrancar el test con:
+#
+#   oe -Q gapp_logistic_connector -c botella -d botella_test
+#
+
+
 from openerp.tests.common import HttpCase, TransactionCase
 
 
