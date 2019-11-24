14cdf5657afc2ccf86fee94ab0784d774468c161
jobiols <jorge.obiols@gmail.com>
2019-11-24 13:45:50 -0300

    [DOC] mejorar documentacion

--- scaffold_module/tests/test_something.py
+++ scaffold_module/tests/test_something.py
@@ -1,6 +1,23 @@
 # Copyright <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+#   Para correr los tests
+#
+#   Definir un subpackage tests que será inspeccionado automáticamente por
+#   modulos de test los modulos de test deben enpezar con test_ y estar
+#   declarados en el __init__.py, como en cualquier package.
+#
+#   Hay que crear una base de datos para testing como sigue:
+#   - Nombre sugerido: [nombre cliente]_test
+#   - Debe ser creada con Load Demostration Data chequeado
+#   - Usuario admin y password admin
+#   - El modulo que se quiere testear debe estar instalado.
+#
+#   Arrancar el test con:
+#
+#   oe -Q [modulo-a-testear-] -c [cliente] -d [cliente]_test
+#
+
 from openerp.tests.common import HttpCase, TransactionCase
 
 
