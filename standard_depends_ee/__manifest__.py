1a706606f4a67592a62fffc5d3fbb0cd26308444
jobiols <jorge.obiols@gmail.com>
2020-03-12 13:02:04 -0300

    [MIG] standard_depends_ee

--- standard_depends_ee/__manifest__.py
+++ standard_depends_ee/__manifest__.py
@@ -20,7 +20,7 @@
 
 {
     'name': 'Standard Dependencies EE',
-    'version': '13.0.0.1.0',
+    'version': '13.0.0.0.0',
     'category': 'Tools',
     'summary': "Add standard dependecies for Argentinian localization",
     'author': "jeo Software",
@@ -28,39 +28,42 @@
     'license': 'AGPL-3',
     'depends': [
         # para la localizacion argentina version Enterprise
-        'l10n_ar_base',  # esto se instala solo
-        'l10n_ar_account',  # esto se instala solo
-        'l10n_ar_afipws_fe',  # Factura Electrónica Argentina
-        'l10n_ar_aeroo_einvoice',  # impresion de factura electronica aeroo
-        'l10n_ar_account_vat_ledger_citi',  # Reportes citi
-        'account_debt_management',  # mejoras en administracion de deudas
-        'l10n_ar_aeroo_payment_group',  # Pagos
-        'l10n_ar_account_withholding',  # Retenciones
-        'account_accountant',           # permisos para contabilidad
-        'l10n_ar_aeroo_stock',   # remito argentino
+        'l10n_ar',  # esto se instala solo
+        'account_payment_group',
+        'account_check',
+        #'l10n_ar_base',  # esto se instala solo
+        #'l10n_ar_account',  # esto se instala solo
+        #'l10n_ar_afipws_fe',  # Factura Electrónica Argentina
+        #'l10n_ar_aeroo_einvoice',  # impresion de factura electronica aeroo
+        #'l10n_ar_account_vat_ledger_citi',  # Reportes citi
+        #'account_debt_management',  # mejoras en administracion de deudas
+        #'l10n_ar_aeroo_payment_group',  # Pagos
+        #'l10n_ar_account_withholding',  # Retenciones
+        #'account_accountant',           # permisos para contabilidad
+        #'l10n_ar_aeroo_stock',   # remito argentino
 
         #'base_currency_inverse_rate',  # poner el tipo de cambio en Argentino chequear que no ponga multicurrency de prepo
 
         # utilitarios
-        'account_ux',  # hace pilas de cosas ver en el modulo
-        'base_ux',  # mejoras de base
-        'mass_editing',  # permite hacer edicion masiva
-        'product_ux',  # mejoras en productos
-        'sale_ux',  # mejoras en ventas
-        'auto_backup',  # poner el backup en: /var/odoo/backups/
-        'mail_activity_board_ux',  # quitar actividades del chatter
-        'partner_ref_unique',  # evita duplicados en referencia
-        'partner_vat_unique',  # evita duplicados numeros de referencia
-        'product_unique',  # no se pueden duplicar codigos de producto
-        'web_export_view',  # exportar cualquier vista en excel
-        'account_clean_cancelled_invoice_number',  # borrar nro de factura
+        #'account_ux',  # hace pilas de cosas ver en el modulo
+        #'base_ux',  # mejoras de base
+        #'mass_editing',  # permite hacer edicion masiva
+        #'product_ux',  # mejoras en productos
+        #'sale_ux',  # mejoras en ventas
+        #'auto_backup',  # poner el backup en: /var/odoo/backups/
+        #'mail_activity_board_ux',  # quitar actividades del chatter
+        #'partner_ref_unique',  # evita duplicados en referencia
+        #'partner_vat_unique',  # evita duplicados numeros de referencia
+        #'product_unique',  # no se pueden duplicar codigos de producto
+        #'web_export_view',  # exportar cualquier vista en excel
+        #'account_clean_cancelled_invoice_number',  # borrar nro de factura
 
         # fixes
         #'invoice_lines_analysis_fix',  # corrige bug en pivot /// falla con la actualizacion
     ],
     'data': [],
     'demo': [],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
