205c8d84212b3de5792406198d14e4de90c03755
Jorge Obiols <jorge.obiols@gmail.com>
2020-08-04 16:17:48 -0300

    [WIP] standard_depends ce

--- standard_depends_ce/__manifest__.py
+++ standard_depends_ce/__manifest__.py
@@ -30,15 +30,20 @@
     'depends': [
         # para la localizacion argentina version Community
         'l10n_ar', # el modulo ADHOC que viene con Odoo
-        'l10n_ar_afipws', 
-
-#        'l10n_ar_edi', # Factura electronica Argentina
-#        'l10n_ar_ux', # Mejoras para argentina, cheques y medios de pago
-#        'l10n_ar_bank',  # Bancos Argentinos
-#        'l10n_ar_reports', # Libros de IVA Digital (ENTERPRISE)
-#        'l10n_ar_stock', # Remitos COT y req Argentinos
-
-        # 'l10n_ar_account_withholding' # Este tiene problemas
+        'l10n_ar_bank',  # Bancos Argentinos
+        'l10n_ar_afipws', # Modulo Base para los Web Services de AFIP
+        'l10n_ar_afipws_fe', # Factura electronica Argentina
+        'l10n_ar_report_fe', # Reportes FE
+        'l10n_ar_account_iva_digital', # Libro de iva digital
+        'l10n_ar_ux', # Mejoras para argentina
+        'l10n_ar_report_payment_group', # Multiples medios de pago
+        'account_check', # Cartera de cheques
+        'l10n_ar_sale_additional_taxes', # Le falta dependencia account_check
+        'l10n_ar_report_payment', # Reportes en pagos
+        'l10n_ar_report_withholding', # Retenciones
+        # 'l10n_ar_stock', # Remito electrónico Argentino tiene una rara dependencia con l10n_ar_account
+        
+        'l10n_ar_report_stock', # OJO esto instala Stock
 
         # Para mejorar la usabilidad
         # 'base_currency_inverse_rate',  # TC en Argentino
