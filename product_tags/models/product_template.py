92e17287a432a9425064c5115e6c54edd3995d5f
Jorge Obiols <jorge.obiols@gmail.com>
2020-08-16 13:47:44 -0300

    [ADD][MIG] product_tag

---
+++ product_tags/models/product_template.py
@@ -0,0 +1,17 @@
+# Copyright 2020 jeo Software
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, tools, SUPERUSER_ID, _
+
+
+class Product(models.Model):
+    _inherit = "product.template"
+
+    tag_id = fields.Many2many(
+        'product.tag',
+        column1='product_id',
+        column2='tag_id',
+        string='Tags',
+        help='Classify or analyze your products with tags.')
+
+
